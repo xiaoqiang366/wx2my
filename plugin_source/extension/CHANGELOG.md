bf94c22fe831ac290e714bd863fc6bc2c02360f7
敖晓强 <aoxiaoqiang@163.com>
2017-11-20 17:58:15 +0800

    fix:

--- plugin_source/extension/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Change Log
-
-## [0.0.3]
-- Add the bridge of wx API
-## [0.0.1]
-- Initial release
--- plugin_source/extension/refineNpmVersion.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find . -iname "package.json" |xargs perl -pi -e "s/\"version\": \"([\w\/:\-\.]+)\"/\"version\":\"0\.1\.0\"/gi"
